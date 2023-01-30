0a70cd7747bfe4043b3837f905466a89afecba7b
Farry1 <47053890+Farry1@users.noreply.github.com>
2023-01-30 22:09:50 +0100

    Some Tree Radius Indication

---
+++ Source/GGJ2023/Data/PlaceableObjectData.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "PlaceableObjectData.generated.h"
+
+/**
+ * 
+ */
+UCLASS(BlueprintType, Blueprintable)
+class GGJ2023_API UPlaceableObjectData : public UDataAsset
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FText DisplayName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UTexture2D* DisplayIcon;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TSubclassOf<AActor> BuildableClass;
+};

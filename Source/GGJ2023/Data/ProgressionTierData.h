6de1c50ac0f09980c9725fcbf73c1aa49c0e5186
Farry1 <47053890+Farry1@users.noreply.github.com>
2023-01-31 23:05:54 +0100

    Some Basic Progression

---
+++ Source/GGJ2023/Data/ProgressionTierData.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "UObject/Object.h"
+#include "ProgressionTierData.generated.h"
+
+/**
+ * 
+ */
+UCLASS(BlueprintType, Blueprintable)
+class GGJ2023_API UProgressionTierData : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FText DisplayName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	int32 PointsNeeded;
+};

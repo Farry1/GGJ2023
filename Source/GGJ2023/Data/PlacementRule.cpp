44fbf0751c4029ba0628e4cdce24378cd66a2499
Farry1 <47053890+Farry1@users.noreply.github.com>
2023-02-01 23:28:57 +0100

    Some work on placement rules

---
+++ Source/GGJ2023/Data/PlacementRule.cpp
@@ -0,0 +1,9 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlacementRule.h"
+
+EPlacementRuleResult UPlacementRule::CanBePlaced_Implementation(AActor* OwningActor)
+{
+	return Fail;
+}
---
+++ Source/GGJ2023/Data/PlacementRule.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "PlacementRule.generated.h"
+
+UENUM(BlueprintType)
+enum EPlacementRuleResult
+{
+	Fail UMETA(DisplayName = "Fail"),
+	Passed UMETA(DisplayName = "Passed"),
+	Perfect UMETA(DisplayName = "Perfect"),
+};
+
+/**
+ * 
+ */
+UCLASS(BlueprintType, Blueprintable)
+class GGJ2023_API UPlacementRule : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	EPlacementRuleResult CanBePlaced(AActor* OwningActor);
+};
